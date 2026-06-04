2c6230e13ffcc87d8c4756246303abc0ba7d3b90
WHB139426 <20000904whb@gmail.com>
2026-06-04 01:45:53 -0700

    update

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,7 +1,7 @@
 export MODEL_ID='/your/path/to/GeoVR-Qwen3-VL-2B'
 export DATA_DIR="/your/path/to/VSI-Bench"
 
-export NUM_GPUS=2
+export NUM_GPUS=1
 
 export CAMERA=False
 export DEPTH=False
@@ -9,6 +9,6 @@ export DISTILL=False
 export SCALE=False
 
 export NUM_FRAME=128
-CUDA_VISIBLE_DEVICES=0,1 python eval_vsi.py
+CUDA_VISIBLE_DEVICES=0 python eval_vsi.py
 
 python metric_result.py
